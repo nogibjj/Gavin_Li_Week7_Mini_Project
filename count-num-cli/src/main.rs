3c1543bae3aaafdcfe677cdf3276a1fcf37b89bf
GLi81 <90353988+gli81@users.noreply.github.com>
2023-10-13 00:01:03 +0000

    moved code to lib.rs & created test & reformat

--- count-num-cli/src/main.rs
+++ count-num-cli/src/main.rs
@@ -1,22 +1,6 @@
-use std::collections::HashMap;
 use clap::Parser;
-
-fn logic(numbers: Vec<i32>) -> Vec<(i32, u32)> {
-    let mut frequencies = HashMap::new();
-
-    for num in numbers {
-        let frequency = frequencies.entry(num).or_insert(0);
-        *frequency += 1;
-    }
-
-    let mut result = Vec::new();
-
-    for (num, frequency) in frequencies {
-        result.push((num, frequency));
-    }
-
-    result
-}
+// use std::collections::HashMap;
+use count_num_cli::{logic, string_to_list};
 
 /// cli tool
 #[derive(Parser, Debug)]
@@ -26,17 +10,10 @@ struct Args {
     #[arg(short, long)]
     default: bool,
 
-    #[arg(short,long,default_value="None")]
+    #[arg(short, long, default_value = "None")]
     cust_list: String,
 }
 
-fn string_to_list(string: String) -> Vec<i32> {
-    let numbers: Vec<i32> = string.split(',')
-                                  .filter_map(|n| n.parse().ok())
-                                  .collect();
-    println!("{:?}", numbers);
-    numbers
-}
 
 fn main() {
     let defualt_nums = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 3];
@@ -44,13 +21,19 @@ fn main() {
     if args.default {
         let result = logic(defualt_nums);
         println!("The count of each number is {:?}", result);
+        // return result;
+    } else if args.cust_list == "None" {
+            println!(
+                "Please use --default flat to count numbers in the default list, or use --cust-list flag to input customized list"
+            );
+            // return None;
     } else {
-        if args.cust_list == "None" {
-            println!("Please use default list, or use --cust-list flag to input customized list");
-        } else {
-            // deal with the string list
-            println!("The count of each number is {:?}",
-                     logic(string_to_list(args.cust_list)));
-        }
+        // deal with the string list
+        let result = logic(string_to_list(args.cust_list));
+        println!(
+            "The count of each number is {:?}",
+            result
+        );
+        // return result;
     }
 }
